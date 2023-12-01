eef88bae3361e4be362760dca6c218697d973f2c
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-01 20:22:43 -0300

    renegando con el responsive

--- Client/src/components/SearchBar/SearchBar.jsx
+++ Client/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,11 @@
-import {React, useState} from "react"
-import { Link } from 'react-router-dom'
-import Logo from "../../assets/TLlogoAlpha.png"
-import userAvatar from '../../assets/userAvatar.png'
-import storeAvatar from '../../assets/storeAvatar.png'
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/TLlogoAlpha.png";
+import userAvatar from "../../assets/userAvatar.png";
+import storeAvatar from "../../assets/storeAvatar.png";
 import style from "./SearchBar.module.css";
 
 const SearchBar = () => {
-
   const [showNotifications, setShowNotifications] = useState(false);
 
   const toggleNotifications = () => {
@@ -18,36 +17,38 @@ const SearchBar = () => {
       <div className={style.background}>
         <img src={Logo} alt="logo" className={style.logo} />
         <button className={style.notifications} onClick={toggleNotifications}>
-        <img
-          width="30"
-          height="30"
-          src="https://img.icons8.com/ios-filled/50/FFFFFF/appointment-reminders--v1.png"
-          alt="appointment-reminders--v1"
-          className={style.bell}
-        />
+          <img
+            width="30"
+            height="30"
+            src="https://img.icons8.com/ios-filled/50/FFFFFF/appointment-reminders--v1.png"
+            alt="appointment-reminders--v1"
+            className={style.bell}
+          />
         </button>
       </div>
       {showNotifications && (
         <div className={style.modal}>
           <div>
-          <button className={style.notifAcces}>
-          <img src={userAvatar} alt="" />
-          <p>Notificación ejemplo 1</p>
-          </button>
+            <button className={style.notifAcces}>
+              <img src={userAvatar} alt="" />
+              <p>Notificación ejemplo 1</p>
+            </button>
           </div>
           <div>
-          <button className={style.notifAcces}>
-          <img src={userAvatar} alt="" />
-          <p>Notificación ejemplo 2</p>
-          </button>
+            <button className={style.notifAcces}>
+              <img src={userAvatar} alt="" />
+              <p>Notificación ejemplo 2</p>
+            </button>
           </div>
           <div>
-          <button className={style.notifAcces}>
-          <img src={storeAvatar} alt="" />
-          <p>Notificación ejemplo 3</p>
-          </button>
+            <button className={style.notifAcces}>
+              <img src={storeAvatar} alt="" />
+              <p>Notificación ejemplo 3</p>
+            </button>
           </div>
-          <button onClick={toggleNotifications} className={style.close}>Cerrar</button>
+          <button onClick={toggleNotifications} className={style.close}>
+            Cerrar
+          </button>
         </div>
       )}
       <div className={style.searchBar}>
--- Client/src/views/Home/Home.jsx
+++ Client/src/views/Home/Home.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import Banner from '../../components/Banners/Banners'
+import React from "react";
+import Banner from "../../components/Banners/Banners";
+import Cards from "../../components/Cards/Cards";
+import fuego from "../../assets/fuego.gif";
+import style from "./Home.module.css";
 
 const Home = () => {
   return (
     <>
-    <Banner />
-    <div>Soy la view de Home!</div>
+      <Banner />
+      <div className={style.home}>
+        {/* <div className={style.fire}>
+          <h2>Destacados</h2>
+          <img src={fuego} alt="fuego" />
+        </div> */}
+        <div>
+          <Cards/>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
