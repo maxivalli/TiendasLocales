4f8ad83d0e98fa133f6c296b10aba2ff82a9b92b
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-08 19:01:55 -0300

    delete pretty chat

--- Client/src/views/Messages/Messages.jsx
+++ Client/src/views/Messages/Messages.jsx
@@ -1,62 +1,34 @@
-import { React, useState, useEffect } from "react";
-import Head from "../../components/Head/Head";
+import { React } from "react";
 import { useSelector } from "react-redux";
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { ChatEngine } from "react-chat-engine";
 import "./Messages.css";
 
 const Messages = () => {
   const userData = useSelector((state) => state.userData);
 
-  const [showModal, setShowModal] = useState(false);
-  const [chatClass, setChatClass] = useState("chat");
-
-  const toggleModal = () => {
-    setShowModal((prevState) => !prevState);
-    setChatClass((prevClass) => (prevClass === "chat" ? "chat2" : "chat"));
-  };
-
-  const buttonLabel =
-    chatClass === "chat2" ? (
-      <img
-        width="28"
-        height="28"
-        src="https://img.icons8.com/sf-regular-filled/48/FA5252/chat.png"
-        alt="chat"
-      />
-    ) : (
-      <img
-        width="22"
-        height="22"
-        src="https://img.icons8.com/material-rounded/24/737373/menu--v1.png"
-        alt="menu--v1"
-      />
-    );
-
-  useEffect(() => {
-    if (window.innerWidth < 768) {
-      setChatClass("chat2");
-    } else {
-      setChatClass("chat");
-    }
-  }, []);
-
   return (
     <>
-      <div className={chatClass}>
-        <button onClick={toggleModal} className="toggle">
-          {buttonLabel}
-        </button>
-        <div className="icon">👉🏻</div>
-        <PrettyChatWindow
-          projectId="1fb49778-0ca9-4761-a91b-512f3a51ee7f"
-          username={userData.username}
-          secret={userData.email}
-          avatarUrl={userData.image}
+      <div className="chat">
+        <ChatEngine
+          projectID="1fb49778-0ca9-4761-a91b-512f3a51ee7f"
+          userName={userData.username}
+          userSecret={userData.email}
           height="calc(100vh - 60px)"
+          offset={-3}
         />
       </div>
     </>
   );
 };
+
+{
+  /* <PrettyChatWindow
+  projectId="1fb49778-0ca9-4761-a91b-512f3a51ee7f"
+  username={userData.username}
+  secret={userData.email}
+  avatarUrl={userData.image}
+  height="calc(100vh - 60px)"
+/>; */
+}
 
 export default Messages;
