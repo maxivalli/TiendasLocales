51d5cc083de90237de16a64b8435684583b53f85
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-13 23:09:03 -0300

    PWA test

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,86 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssets: [
+    "favicon.ico",
+    "apple-touc-icon.png",
+    "maskable_icon.png",
+    "android-chrome-192x192.png",
+    "android-chrome-512x512.png",
+  ],
+  manifest: {
+    name: "Tiendas Locales",
+    short_name: "Tiendas Locales",
+    description: "¡Comprá en tu ciudad!",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+
+    shortcuts: [
+      {
+        name: "Agregar",
+        description: "Agregar un producto",
+        url: "https://tiendaslocales.com.ar/#/addproduct",
+        icons: [
+          {
+            src: "add.png",
+            sizes: "192x192",
+          },
+        ],
+      },
+      {
+        name: "Mi perfil",
+        description: "Mi perfil",
+        url: "https://locanjeamos.com.ar/#/account",
+        icons: [
+          {
+            src: "user.png",
+            sizes: "192x192",
+          },
+        ],
+      },
+    ],
+
+    theme_color: "#ff0000",
+    background_color: "#ff0000",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugIn)],
+  base: "",
+  build: {
+    chunkSizeWarningLimit: 1000000,
+  },
+});
