bf2a95249a0e8f4295d5deabe4ac89eb5f847805
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-18 00:52:17 -0300

    merge

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -32,8 +32,8 @@ let socket;
 
 function App() {
   const dispatch = useDispatch();
-  //axios.defaults.baseURL = "http://localhost:3001/";
-  axios.defaults.baseURL = "https://tiendaslocales-production.up.railway.app/";
+  axios.defaults.baseURL = "http://localhost:3001/";
+  //axios.defaults.baseURL = "https://tiendaslocales-production.up.railway.app/";
   const {
     user,
     isAuthenticated: isAuthenticatedAuth0,
@@ -196,8 +196,8 @@ function App() {
 
   useEffect(() => {
     if (isAuthenticated) {
-      socket = io("https://tiendaslocales-production.up.railway.app/");
-      //socket = io("http://localhost:3001/")
+      //socket = io("https://tiendaslocales-production.up.railway.app/");
+      socket = io("http://localhost:3001/")
       setShouldConnectSocket(true);
     }
   }, [isAuthenticated]);
