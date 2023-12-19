8f7298af621f46d478bb898eacf7b2aaf19f7983
Santino Mantineo <santinomantineo@gmail.com>
2023-12-19 01:48:41 -0300

    changes

--- Server/src/controllers/payController.js
+++ Server/src/controllers/payController.js
@@ -4,13 +4,14 @@ const { ACCESS_TOKEN, CLIENT_ID, CLIENT_SECRET } = process.env;
 const mercadopago = require("mercadopago");
 const axios = require("axios");
 
-mercadopago.configure({
-  access_token:  ACCESS_TOKEN
-});
+
 exports.createOrder = async (paymentData) => {
     try{
+      
+      mercadopago.configure({
+        access_token:  ACCESS_TOKEN
+      });
 
-        console.log(paymentData.accT)
         let preference = {
             items: [{
                 postId: paymentData.postId,
@@ -26,7 +27,7 @@ exports.createOrder = async (paymentData) => {
                 pending: "http://localhost:5173/#/account",
                 success: "http://localhost:5173/#/account"
             },
-            notification_url: "https://204d-201-190-251-186.ngrok-free.app/tiendas/webhook"
+            notification_url: "https://9c20-201-190-251-186.ngrok-free.app/tiendas/webhook"
         }
 
         const response = await mercadopago.preferences.create(preference);
@@ -76,34 +77,6 @@ console.log(allData)
           };
       }
   }
-// exports.webhook = async (allData) => {
-    // if (allData.data.type === "payment") {
-
-    //     const post = await Post.findOne({
-    //         where: {
-    //             id: allData.payUserData.postId,
-    //         },
-    //       });
-
-    //     post.stock = post.stock - allData.payUserData.quantity;
-    //     await post.save();
-
-    //     const newCompra = await Compra.create({
-    //         userId: allData.payUserData.userId,
-    //         postId: allData.payUserData.postId,
-    //         storeId: post.storeId,
-    //         title: allData.payUserData.title,
-    //         quantity: allData.payUserData.quantity,
-    //         unit_price: allData.payUserData.unit_price,
-    //         currency_id: allData.payUserData.currency_id,
-    //         description: allData.payUserData.description,
-    //         productImage: post.image
-    //       });
-//     } else {
-//         throw new Error("Invalid webhook event type");
-//     }
-// }
-
 
 exports.allCompras = async (id) => {
 
@@ -150,7 +123,7 @@ exports.accT = async (code, state) => {
           client_secret: 'dbj3rL8bNBQ6UOzxaI4nOEjTcC22yAMa',
           code: code,
           grant_type: 'authorization_code',
-          redirect_uri: 'https://204d-201-190-251-186.ngrok-free.app/tiendas/redirectUrl',
+          redirect_uri: 'https://9c20-201-190-251-186.ngrok-free.app/tiendas/redirectUrl',
           test_token: true,
         }),
       });
@@ -158,7 +131,6 @@ exports.accT = async (code, state) => {
       const data = await response.json();
       const accessToken = data.access_token;
   
-      // Ahora puedes hacer algo con el accessToken, por ejemplo, guardarlo en la base de datos para el usuario.
       const user = await User.findOne({
         where: {
           id: state,
