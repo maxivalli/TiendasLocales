b54b557449fcbc1f66746ef7382ce7f3c4e5f124
Santino Mantineo <santinomantineo@gmail.com>
2023-12-19 17:31:44 -0300

    cambios redirect url

--- Server/src/routes/tiendasRoutes/tiendasRoutes.js
+++ Server/src/routes/tiendasRoutes/tiendasRoutes.js
@@ -164,13 +164,9 @@ router.get("/redirectUrl", async (req, res) => {
   const { code, state } = req.query;
 
   try {
-    // que aca reciba el code, genere el acces token y te lleve a more de tiendas locales.
-    // que pregunte en un boton donde dice espera si el usuario tiene access token, si no lo tiene que le aparezca el boton que lo lleva al link, y si lo tiene que muestre 1/2
-    // Que a donde dice en espera diga 0/2 si no se hizo el acces token y 2/2 si se hizo el token y se pago a la empresa.
-    // que lo redirija al usuario a more ahora mientras procesa el token
     const response = await payController.accT(code, state);
     if (response) {
-      return true;
+      return res.status(200).json("Token generado correctamente! Vuelve al sitio para continuar con tu proceso de aprobacion.");
     }
   } catch (error) {
     return res.status(400).json(error.message);
