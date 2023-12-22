21999500d3f0cad6db153bcb6b44d695a1932a81
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-22 00:48:20 -0300

    text async

--- Client/src/views/Messages/Messages.jsx
+++ Client/src/views/Messages/Messages.jsx
@@ -48,35 +48,33 @@ const Messages = () => {
     ? storeEmail
     : savedStoreData.email;
 
-  const requestNotificationPermission = () => {
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        setPermissionGranted(true); 
-      }
-    });
-  };
-
   useEffect(() => {
-    const activarMensajes = async () => {
-      if (permissionGranted) {
-        const token = await getToken(messaging, {
-          vapidKey:
-            "BNY5OiGgDKe6EVWr76IohPCDDrKwCdr48QVhp9K5T1CdCDYkJ3dUbUl2ciToadj8OPGO2JTpPaEA7kwXe4w0aMA",
-        }).catch((error) => console.log("Error al generar el token", error));
-        if (token) {
-          console.log("tu token: ", token);
-          userData.FCMtoken = token;
-          const id = userData.id;
-          dispatch(updateUser(id, userData));
+    const requestNotificationPermission = () => {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          console.log("permiso garantizado");
         }
-        if (!token) console.log("no hay token");
-      }
+      });
     };
-
     requestNotificationPermission();
-
     activarMensajes();
-  }, [permissionGranted]);
+  });
+
+  const activarMensajes = async () => {
+    if (permissionGranted) {
+      const token = await getToken(messaging, {
+        vapidKey:
+          "BNY5OiGgDKe6EVWr76IohPCDDrKwCdr48QVhp9K5T1CdCDYkJ3dUbUl2ciToadj8OPGO2JTpPaEA7kwXe4w0aMA",
+      }).catch((error) => console.log("Error al generar el token", error));
+      if (token) {
+        console.log("tu token: ", token);
+        userData.FCMtoken = token;
+        const id = userData.id;
+        dispatch(updateUser(id, userData));
+      }
+      if (!token) console.log("no hay token");
+    }
+  };
 
   useEffect(() => {
     const updateDOMElements = () => {
