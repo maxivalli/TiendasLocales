4d38a4ce5017ed7598f28e7f6cb1bbd9b493d4d5
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-21 21:41:05 -0300

    test icon

--- Client/src/firebase-messaging-sw.js
+++ Client/src/firebase-messaging-sw.js
@@ -5,9 +5,9 @@ importScripts(
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js"
 );
 
-import { precacheAndRoute } from 'workbox-precaching'
+import { precacheAndRoute } from "workbox-precaching";
 
-precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST);
 
 const firebaseConfig = {
   apiKey: "AIzaSyAiXwnw-mIc8s-DkV2y8JaSwjKa__KF6i8",
@@ -27,7 +27,26 @@ messaging.onBackgroundMessage((payload) => {
 
   return self.registration.showNotification(payload.data.title, {
     body: payload.data.text,
-    tag: 'Tiendas Locales',
-    icon: './assets/logo.png'
+    icon: "https://firebasestorage.googleapis.com/v0/b/tiendaslocales-7bbf8.appspot.com/o/logo.png?alt=media&token=bca80e33-79d3-4b7e-8e50-e7cb026a2a58",
   });
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close(); 
+  
+  const url = 'https://tiendaslocales.com.ar'; 
+  
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then(windowClients => {
+
+      for (let client of windowClients) {
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
