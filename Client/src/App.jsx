6155e628882e4295387881601b27b324d1a7a8d3
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-31 01:27:45 -0300

    call sw

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -232,6 +232,21 @@ function App() {
     }
   }, [shouldConnectSocket, userId]);
 
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("/sw.js")
+          .then((registration) => {
+            console.log("Service Worker registrado con éxito: ", registration);
+          })
+          .catch((err) => {
+            console.error("Error al registrar el Service Worker: ", err);
+          });
+      });
+    }
+  }, []);  
+
   const [isSlowConnection, setIsSlowConnection] = useState(false);
 
   useEffect(() => {
