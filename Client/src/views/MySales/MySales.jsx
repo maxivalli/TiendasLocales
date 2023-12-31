f0ef19a0739b805be6a70388612eda410fb84f1c
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2023-12-31 16:40:35 -0300

    categ

--- Client/src/views/MySales/MySales.jsx
+++ Client/src/views/MySales/MySales.jsx
@@ -11,46 +11,44 @@ import style from "./MySales.module.css";
 import { setSelectedStore } from "../../redux/actions";
 
 const MySales = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const userData = useSelector((state) => state.userData);
   const stores = useSelector((state) => state.allStores);
-  const userStore = stores.find((store) => store.userId === userData.id)
+  const userStore = stores.find((store) => store.userId === userData.id);
   const users = useSelector((state) => state.allUsers);
   const [comprasData, setCompras] = useState([]);
-  const comprasEnviadas = comprasData.filter((item) => item.enviado === true)
-  const comprasPorEnviar = comprasData.filter((item) => item.enviado === false)
+  const comprasEnviadas = comprasData.filter((item) => item.enviado === true);
+  const comprasPorEnviar = comprasData.filter((item) => item.enviado === false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-          const response = await axios.get(
-            `/tiendas/comprasRecibidas/${userStore?.id}`
-          );
-          if (response) {
-            setCompras(response.data);
-          }
+        const response = await axios.get(
+          `/tiendas/comprasRecibidas/${userStore?.id}`
+        );
+        if (response) {
+          setCompras(response.data);
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-  
-    fetchData();
-  }, [userStore]); 
-  
 
-  const handleEnviado = async (itemId) =>{
-    try{
+    fetchData();
+  }, [userStore]);
+
+  const handleEnviado = async (itemId) => {
+    try {
       const response = await axios.put(`/tiendas/enviado/${itemId}`);
-      if(response){
-        socket?.emit("productoEnviado", itemId)
-       
-        return true
+      if (response) {
+        socket?.emit("productoEnviado", itemId);
+
+        return true;
       }
-    } catch(error){
+    } catch (error) {
       console.error("Error fetching data:", error);
     }
-  }
-
+  };
 
   return (
     <>
@@ -59,31 +57,38 @@ const MySales = () => {
       <div className={style.misventas}>
         <h2 className={style.title}>Mis ventas</h2>
 
-        <h3>Pendientes</h3>
-        <div className={style.pendientes}>
-          {comprasPorEnviar && comprasPorEnviar.map((item, index) => {
-            return (
-              <CardSale
-                key={index}
-                id={item?.id}
-                image={item?.productImage}
-                title={item?.title}
-                price={item?.unit_price}
-                quantity={item?.quantity}
-                delivery={item?.delivery}
-                enviado={item?.enviado}
-                user={users && users.find((user) => user.id === item?.userId)}
-                adress={item?.userDireccion}
-                fn={()=>{handleEnviado(item?.id)}}
-              />
-            );
-          })}
-        </div>
+        <h3 className={style.pend}>Pendientes de envío</h3>
 
-        <h3>Enviadas o entregadas</h3>
-        <div className={style.entregadas}>
-        {comprasEnviadas && comprasEnviadas.map((item, index) => {
-            return (
+        {comprasPorEnviar && comprasPorEnviar.length > 0 ? (
+          <div className={style.pendientes}>
+            {comprasPorEnviar.map((item, index) => (
+              <CardSale
+                key={index}
+                id={item?.id}
+                image={item?.productImage}
+                title={item?.title}
+                price={item?.unit_price}
+                quantity={item?.quantity}
+                delivery={item?.delivery}
+                enviado={item?.enviado}
+                user={users && users.find((user) => user.id === item?.userId)}
+                address={item?.userDireccion}
+                fn={() => {
+                  handleEnviado(item?.id);
+                }}
+              />
+            ))}
+          </div>
+        ) : (
+          <div className={style.nopend}>
+            <p>No hay compras pendientes de envío.</p>
+          </div>
+        )}
+
+        <h3 className={style.ent}>Enviadas</h3>
+        {comprasEnviadas && comprasEnviadas.length > 0 ? (
+          <div className={style.entregadas}>
+            {comprasEnviadas.map((item, index) => (
               <CardSale
                 key={index}
                 id={item?.id}
@@ -94,12 +99,18 @@ const MySales = () => {
                 enviado={item?.enviado}
                 delivery={item?.delivery}
                 user={users && users.find((user) => user.id === item?.userId)}
-                adress={item?.userDireccion}
-                fn={()=>{handleEnviado(item?.id)}}
+                address={item?.userDireccion}
+                fn={() => {
+                  handleEnviado(item?.id);
+                }}
               />
-              );
-            })}
-        </div>
+            ))}
+          </div>
+        ) : (
+          <div className={style.noent}>
+            <p>Aún no hay compras enviadas.</p>
+          </div>
+        )}
       </div>
     </>
   );
