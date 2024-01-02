314f4fa1e3fa01016f5691cdec337c43b487b729
Maximiliano Valli <127133282+maxivalli@users.noreply.github.com>
2024-01-02 15:00:24 -0300

    open stores button

--- Client/src/components/Filters/Filters.jsx
+++ Client/src/components/Filters/Filters.jsx
@@ -120,11 +120,16 @@ const Filters = () => {
               <option value="asc">A-Z</option>
               <option value="desc">Z-A</option>
             </select>
-            {!location.hash.includes("/mitienda/") &&
-              !location.hash.includes("/tienda/") &&
-              !location.hash.includes("/misventas") && (
-            <button onClick={showOpenStores}>{botonTiendasAbiertas}</button>
-            )}
+            <button
+              onClick={showOpenStores}
+              className={
+                isShowOpenStoresPressed
+                  ? `${style.todas} ${style.todas}`
+                  : style.abiertas
+              }
+            >
+              {botonTiendasAbiertas}
+            </button>
             <button onClick={toggleFilters}>Hecho</button>
             <button onClick={handleResetFilters} className={style.limpiar}>
               Limpiar filtros
