bd7568db71c19f301e8748af48ceb5619e2af929
Carlos Emanuel Klema <carlosklema@hotmail.com>
2024-01-04 13:32:02 -0300

    lpm

--- Client/src/redux/actions.js
+++ Client/src/redux/actions.js
@@ -64,7 +64,6 @@ import {
 export function getAllUsers() {
   return async function (dispatch) {
     const response = await axios("/users/allUsers");
-   
     return dispatch({
       type: GET_ALL_USERS,
       payload: response.data,
